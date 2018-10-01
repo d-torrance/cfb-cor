926b9b17cb4d7f4215bd61c250fcd438a7617051
Doug Torrance <dtorrance@piedmont.edu>
2018-10-01 04:45:11 -0400

    Keep track of W-L record

--- cfb-cor.py
+++ cfb-cor.py
@@ -14,6 +14,9 @@ week = div.get_text(strip=True)
 
 G = nx.DiGraph()
 
+actual_wins = {}
+actual_losses = {}
+
 for i in range(1, int(week) + 1):
     doc = requests.get("https://www.ncaa.com/scoreboard/football/fbs/" + year
                        + "/" + str(i).zfill(2) + "/all-conf")
@@ -26,14 +29,36 @@ for i in range(1, int(week) + 1):
         team2 = teams[1].find("span",
                               class_="gamePod-game-team-name").get_text()
         if teams[0]["class"][0] == "winner":
+            if team1 in actual_wins:
+                actual_wins[team1] += 1
+            else:
+                actual_wins[team1]= 1
+            if team2 in actual_losses:
+                actual_losses[team2] += 1
+            else:
+                actual_losses[team2] = 1
             G.add_edge(team1, team2)
         elif teams[1]["class"][0] == "winner":
+            if team2 in actual_wins:
+                actual_wins[team2] += 1
+            else:
+                actual_wins[team2]= 1
+            if team1 in actual_losses:
+                actual_losses[team1] += 1
+            else:
+                actual_losses[team1] = 1
             G.add_edge(team2, team1)
 
 wins = {}
 
 for team in G:
     wins[team] = 0
+
+for team in G:
+    if team not in actual_wins:
+        actual_wins[team] = 0
+    if team not in actual_losses:
+        actual_losses[team] = 0
 
 for teams in itertools.combinations(G, 2):
     try:
@@ -54,4 +79,6 @@ sorted_wins = list(reversed(sorted(wins.items(), key=lambda kv: kv[1])))
 
 for i in range(1, 26):
     print(str(i) + " - " + sorted_wins[i-1][0] + " (" +
+          str(actual_wins[sorted_wins[i-1][0]]) + "-" +
+          str(actual_losses[sorted_wins[i-1][0]]) + ") (" +
           str(sorted_wins[i-1][1]) + ")")
